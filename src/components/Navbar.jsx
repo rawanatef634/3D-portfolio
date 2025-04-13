66fbe8f2196e29a6fd23094477b8802a3f9d161c
rawanatef634 <atron944@gmail.com>
2025-04-13 12:07:59 +0200

    updated logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <header className='flex justify-between items-center sm:px-16 px-8 py-4 max-w-5xl mx-auto absolute top-0 bg-transparent z-10 right-0 left-0'>
       <NavLink to='/'>
-        <img src={logo} alt='logo' className='w-18 h-18 object-contain' />
+        <img src={logo} alt='logo' className='w-18 h-18 object-contain rounded-2xl' />
       </NavLink>
       <nav className='flex text-xl gap-7 font-medium'>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-white" : "text-gray-800" }>
